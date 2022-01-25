ae038592c5959d79cc902424b2f39c9e8734570f
Zeus <vishaljuneja707@gmail.com>
2022-01-25 23:25:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,25 +9,25 @@
 ######################################################################
 
 sudo apt-get update && sudo apt-get upgrade 
-sudo apt-get install wget curl git vlc python3 python3-pip vim vim-airline* gcc g++-10 fonts-powerline binutils gdb build-essential binwalk 
-wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip ~/Downloads/hack.zip
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ~/Downloads/chrome.deb
-wget https://discord.com/api/download?platform=linux&format=deb ~/Downloads/discord.deb
-wget https://go.microsoft.com/fwlink/?LinkID=760868 ~/Downloads/code.deb 
+sudo apt-get install wget curl git vlc python3 python3-pip vim vim-airline* gcc g++-10 fonts-powerline binutils gdb build-essential binwalk -y 
+wget "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip" -O ~/Downloads/hack.zip
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O ~/Downloads/chrome.deb
+wget "https://discord.com/api/download?platform=linux&format=deb" -O  ~/Downloads/discord.deb
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O ~/Downloads/code.deb 
 sudp wget https://out7.hex-rays.com/files/idafree76_linux.run
 chmod +x ~/Downloads/*
-sudo apt install ~/Downloads/*.deb 
-sudo apt-get install google-chrome-stable discord
+sudo apt install ~/Downloads/*.deb -y 
+sudo apt-get install google-chrome-stable discord -y 
 
 # sublime installation 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text -y 
 
 # spotify installation 
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get install spotify-client -y 
 
 git clone https://github.com/Zeus-HackOlympus/dotfiles ~/dotfiles
 
@@ -45,5 +45,4 @@ bash -c  "$(wget -qO- https://git.io/vQgMr)"
 
 printf "\n\n❗❗add and update ssh keys clone CP, C and Blog Folder❗❗\n""
 echo "done!!" 
-
 
